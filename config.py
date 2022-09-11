1d900acb66681c1100dcc597a4f5ed346df58559
tehmingann <113285920+tehmingann@users.noreply.github.com>
2022-09-11 17:48:57 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.ceqbc6d924as.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "nning-employee"
+customregion = "us-east-1b"
